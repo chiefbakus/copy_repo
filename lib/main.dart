d36222ff48a376c3a0967ba644e67c0ca2e39b5c
chiefbakus <baboubakar005@gmail.com>
2025-08-28 11:45:56 -0400

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,6 @@
-// ignore: unused_import
 import 'package:flutter/material.dart';
-import 'package:first_app/GradientContainer.dart';
 
 
-void main() {
-  runApp(
-  const MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 63, 5, 120),
-        body: GradientContainer(
-          Color.fromARGB(255, 33, 5,109),
-          Color.fromARGB(255, 33, 5,109),
-      ),
-    ),
-  ),
-  );
+void main(){
+  runApp(MaterialApp(home: Text("Hello world")));
 }
-
-
