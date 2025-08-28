8ad1dd701f95a6e0a4f22fbf6484c9776fd9953d
Aboubakar Bakayoko <baboubakar005@gmail.com>
2025-08-28 07:32:35 -0400

    reussable widjet 1

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+// ignore: unused_import
+import 'package:flutter/material.dart';
+import 'package:first_app/GradientContainer.dart';
+
+
+void main() {
+  runApp(
+  const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 63, 5, 120),
+        body: GradientContainer(),
+      ),
+    ),
+  );
+}
+
+
